5e74812312c338576b26d9804b47882c7881f1fd
Ryan Chung <chungryan0803@gmail.com>
2017-02-13 01:07:28 -0500

    some potato early stage test code for GPIOs and PWM and state machine

---
+++ potatoStateMachine.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# initialize GPIOs
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+def state1():
+    while True:
+        print 'state 1'
+        
+        if (GPIO.input(23) == 1):
+            sleep(0.3)
+            return state2()
+            
+def state2():
+    while True:
+        print 'state 2'
+        
+        if (GPIO.input(23) == 1):
+            sleep(0.3)
+            return state1()
+
+state1()
