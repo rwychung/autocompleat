7fdfae09632441b623b2e34360557af7e9154091
Ryan Chung <chungryan0803@gmail.com>
2017-03-01 19:45:15 -0500

    Add init files and changed z axis test.

--- test/driveZAxis.py
+++ test/driveZAxis.py
@@ -7,8 +7,16 @@ import time
 import RPi.GPIO as GPIO
 import Adafruit_PCA9685
 
-import pins
-import config
+# PIN
+Z_AXIS_GPIO_ADDR = 0x20
+Z_AXIS_DIR_PIN = 29
+Z_AXIS_RESET_PIN = 29
+Z_AXIS_STEP_PWM_CHANNEL = 9
+
+# CONFIG
+Z_STEPS_PER_MM = 25
+Z_STEPS_PER_REV = 200
+PWM_PULSE_LENGHT = 4096
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -16,14 +24,14 @@ GPIO.setmode(GPIO.BOARD)
 PWM = Adafruit_PCA9685.PCA9685()
 
 # Setup GPIO to control z axis stepper
-GPIO.setup(pins.Z_AXIS_DIR_PIN, GPIO.OUT)
-GPIO.setup(pins.Z_AXIS_RESET_PIN, GPIO.OUT)
+GPIO.setup(Z_AXIS_DIR_PIN, GPIO.OUT)
+GPIO.setup(Z_AXIS_RESET_PIN, GPIO.OUT)
 
 def mm2Steps(mm):
-    return config.Z_STEPS_PER_MM * mm
+    return Z_STEPS_PER_MM * mm
 
 def speed2Freq(rpm):
-    return config.Z_STEPS_PER_REV * rpm / 60
+    return Z_STEPS_PER_REV * rpm / 60
 
 while True:
     pwmFreq = speed2Freq(10)
@@ -38,8 +46,8 @@ while True:
     """
 
     # Lower board by 100mm
-    GPIO.output(pins.Z_AXIS_DIR_PIN, GPIO.LOW)
-    PWM.set_pwm(pins.Z_AXIS_STEP_PWM_CHANNEL, PWM_PULSE_LENGTH/2, PWM_PULSE_LENGTH/2)
+    GPIO.output(Z_AXIS_DIR_PIN, GPIO.LOW)
+    PWM.set_pwm(Z_AXIS_STEP_PWM_CHANNEL, PWM_PULSE_LENGTH/2, PWM_PULSE_LENGTH/2)
     time.sleep(1 / pwmFreq * mm2Steps(dist))
     input("Press enter to decrease height by 100mm")
 
